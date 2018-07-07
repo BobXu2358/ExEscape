25403967c5d0a1efa6953a76930b0e6b8496c9f5
Xinhai Xu <xxh2358@gmail.com>
2018-07-07 11:30:25 +0800

    initial commit

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public GameObject[] waypoints;
+    private int currentWaypoint = 0;
+    private float lastWaypointSwitchTime;
+    public float speed = 1.0f;
+
+
+    void Start()
+    {
+        lastWaypointSwitchTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //calculate waypoints pos
+        Vector3 startPosition = waypoints[currentWaypoint].transform.position;
+        Vector3 endPosition = waypoints[currentWaypoint + 1].transform.position;
+        //calculate time & distance
+        float pathLength = Vector3.Distance(startPosition, endPosition);
+        float totalTimeForPath = pathLength / speed;
+        float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+        gameObject.transform.position = Vector2.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+        //move
+        if (gameObject.transform.position.Equals(endPosition))
+        {
+            if (currentWaypoint < waypoints.Length - 2)
+            {
+                //switch next waypoint
+                currentWaypoint++;
+                lastWaypointSwitchTime = Time.time;
+                RotateIntoMoveDirection();
+            }
+            else
+            {
+                // level over
+
+            }
+        }
+    }
+
+    private void RotateIntoMoveDirection()
+    {
+        //calculate direction
+        Vector3 newStartPosition = waypoints[currentWaypoint].transform.position;
+        Vector3 newEndPosition = waypoints[currentWaypoint + 1].transform.position;
+        Vector3 newDirection = (newEndPosition - newStartPosition);
+
+        //calculate rotation
+        float x = newDirection.x;
+        float y = newDirection.y;
+        float rot = Mathf.Atan2(y, x) * 180 / Mathf.PI;
+
+        //rotate
+        transform.rotation = Quaternion.AngleAxis(rot, Vector3.forward);
+    }
+}
