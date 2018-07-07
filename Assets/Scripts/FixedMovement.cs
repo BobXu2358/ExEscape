39bb6e98fcbd2c1bbeb06d0b1270eb05a2e2aee0
Xinhai Xu <xxh2358@gmail.com>
2018-07-07 15:03:19 +0800

    get movement done

---
+++ Assets/Scripts/FixedMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedMovement : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    private int i = 0;
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+        i = 0;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 target = new Vector2(waypoints[i].transform.position.x, waypoints[i].transform.position.y);
+
+        //move
+        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), target, Time.deltaTime * speed);
+
+        //reached a waypoint
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            //switch next waypoint
+            i++;
+            transform.Rotate(0, 180, 0);
+        }
+
+    }
+}
